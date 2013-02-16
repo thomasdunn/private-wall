c4365e1aadfa0db95b748af4dc76ebf8338118a7
Tom Dunn <thomas.dunn@gmail.com>
2013-02-16 00:35:53 -0500

    Added checks to TestJavaMail.groovy.  Trying to combine gmail access with writing to DB with TestWriteImage.groovy - not there yet.

--- scripts/TestWriteImage.groovy
+++ scripts/TestWriteImage.groovy
@@ -1,21 +1,13 @@
-
-/**
- * Created with IntelliJ IDEA.
- * User: tdunn
- * Date: 2/2/13
- * Time: 3:30 AM
- * To change this template use File | Settings | File Templates.
- */
-
 import com.sun.mail.gimap.GmailFolder
 import com.sun.mail.gimap.GmailMessage
 import com.sun.mail.gimap.GmailSSLStore
+import privatewall.Content
+import privatewall.User
 
 import javax.mail.*
 
 Properties props = System.getProperties()
 props.setProperty("mail.store.protocol", "gimaps")
-//props.setProperty("mail.debug", "true")
 
 GmailSSLStore store = null
 GmailFolder folder = null
@@ -26,7 +18,7 @@ String password = System.getProperty("password")
 try {
     Session session = Session.getDefaultInstance(props, null)
     store = session.getStore("gimaps") as GmailSSLStore
-    store.connect(username, password) //Constants.getUsername(), Constants.getPassword())
+    store.connect(username, password)
     folder = (GmailFolder) store.getFolder("INBOX")
     folder.open(Folder.READ_ONLY)
     Message[] ms = folder.getMessages()
@@ -82,20 +74,21 @@ def writeAttachments(Message message) {
         BodyPart bodyPart = multipart.getBodyPart(i)
 
         if(! Part.ATTACHMENT.equalsIgnoreCase(bodyPart.getDisposition()) ||
-            ! bodyPart.type.toLowerCase().startsWith("image")) {
+                ! bodyPart.type.toLowerCase().startsWith("image")) {
             continue // dealing with image attachments only
         }
-        InputStream is = bodyPart.getInputStream()
 
-        def fileName = bodyPart.getFileName()
-        f = new File  (System.getProperty("java.io.tmpdir") + fileName).withDataOutputStream {
-            byte[] buf = new byte[4096]
-            int bytesRead
-            while((bytesRead = is.read(buf))!=-1) {
-                it.write(buf, 0, bytesRead)
-            }
-            names << bodyPart.getFileName()
-        }
+        InputStream is = bodyPart.getInputStream()
+        def fromUser = User.findByUserId(message.from[0]?.getAddress())
+
+        Content imageContent = new Content()
+        imageContent.fileName = message.subject
+        imageContent.fileContent = is.getBytes()
+
+        fromUser.addToContents()
+        fromUser.save()
+
+        names << bodyPart.getFileName()
     }
 
     names
